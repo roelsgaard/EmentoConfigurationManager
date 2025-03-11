bde4071561796ff6b95b52ba7077f2ce8fd29d08
Chris Holm Roelsgaard <chris@emento.dk>
2025-03-11 22:31:12 +0100

    .

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-//import stylisticJs from '@stylistic/eslint-plugin-js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
     {
@@ -23,7 +23,7 @@ export default [
         rules: {
             ...reactHooks.configs.recommended.rules,
             "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-            //'@stylistic/js/indent': ['error', 4],
+            '@stylistic/js/indent': ['error', 4],
         },
     },
 ];
